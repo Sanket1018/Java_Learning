2ca70f6ac90e4c3bfb225fcc18b821f8d68a4f5d
KD3_89298_Sanket <sanketkhokadesk@gmail.com>
2025-05-29 20:06:59 +0530

    Array code added

---
+++ Arrays/Arrays.java
@@ -0,0 +1,27 @@
+public class Arrays {
+    public static void main(String[] args) {
+        int a[] = new int[5];
+
+        a[0] = 10;
+        a[1] = 20;
+        a[2] = 30;
+        a[3] = 40;
+        a[4] = 50;
+
+        System.out.println(a[0]);
+        System.out.println(a[1]);
+        System.out.println(a[2]);
+        System.out.println(a[3]);
+        System.out.println(a[4]);
+
+        int arr[] = {10,20,30,40,50};
+
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+        System.out.println(arr[4]);
+
+    }
+
+}
